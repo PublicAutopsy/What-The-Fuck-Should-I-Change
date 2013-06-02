26a5700267b7da6cdfc4dc2d97af935235557b1d
Kevin Mann <kmann@unifiedcompliance.com>
2013-06-01 21:13:04 -0700

    Fixed datasets and added projects

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ var connection = mysql.createConnection({
     host: 'localhost',
     user: 'api',
     password: 'meowmix',
-    database: 'api_repo'
+    database: 'api_repo',
+    multipleStatements: true
 });
 
 connection.connect();
@@ -23,11 +24,11 @@ exports.datasets = function(req, res){
 };
 
 exports.datasetsAdd = function (req, res) {
+    var dsQuery = 'INSERT INTO datasets ()';
     res.render('add_view', { title: 'Add a Dataset', data : true });
 };
 
 exports.datasetSingle = function(req, res){
-    console.log('meow');
     var dataset = req.params.dataset;
     getDataSet( res, dataset );
 };
@@ -36,9 +37,7 @@ exports.datasetSingle = function(req, res){
 
 //Projects start
 exports.project = function (req, res) {
-
-    res.render('list_view', { title: 'Projects' });
-
+    getProjects( res );
 };
 
 exports.projectAdd = function (req, res) {
@@ -47,7 +46,7 @@ exports.projectAdd = function (req, res) {
 
 exports.projectSingle = function (req, res) {
     var project = req.params.project;
-    res.render('content_project_view', { title: 'Single Projects : '+project });
+    getProjects( res, project );
 };
 //projects end
 
@@ -86,8 +85,6 @@ function getDataSet( res, id )
     var query = 'SELECT datasets.*, datasets_to_api_types.api_type_id, api_types.name AS apiname, users.name AS contributer, creators.name AS creator, projects.project_id, projects.name AS project_name, projects.descritiption AS project_description, problems.problem_id, problems.name AS problem_name, problems.description AS problem_description FROM datasets LEFT JOIN users ON datasets.contributer_id = users.user_id LEFT JOIN creators ON datasets.creator_id = creators.creator_id LEFT JOIN datasets_to_api_types ON datasets_to_api_types.dataset_id = datasets.dataset_id LEFT JOIN api_types ON datasets_to_api_types.api_type_id = datasets_to_api_types.api_type_id LEFT JOIN projects_to_datasets ON projects_to_datasets.dataset_id = datasets.dataset_id LEFT JOIN projects ON projects.project_id = projects_to_datasets.project_id LEFT JOIN problems_to_datasets ON problems_to_datasets.dataset_id = datasets.dataset_id LEFT JOIN problems ON problems.problem_id = problems_to_datasets.problem_id';
     if( id !== undefined ) query += ' WHERE datasets.dataset_id = '+ id;
 
-    console.log( query );
-
     connection.query(query, function(err, rows, fields) {
         if( err ) console.log('error: ' + err);
 
@@ -101,9 +98,9 @@ function getDataSet( res, id )
                 holderObj[rows[x].dataset_id].projects = {};
                 holderObj[rows[x].dataset_id].problems = {};
 
-                if( rows[x].api_type_id != null ) holderObj[rows[x].dataset_id].api_types[rows[x].api_type_id] = [{"api_type_id": rows[x].api_type_id, "name": rows[x].apiname}];
-                if( rows[x].project_id != null ) holderObj[rows[x].dataset_id].projects[rows[x].project_id] = [{"project_id": rows[x].project_id, "name": rows[x].project_name, "descritiption": rows[x].projectdescritiption}];
-                if( rows[x].problem_name != null ) holderObj[rows[x].dataset_id].problems[rows[x].problem_id] = [{"problem_id": rows[x].problem_id, "name": rows[x].problem_name, "descritiption": rows[x].problem_description}];
+                if( rows[x].api_type_id != null ) holderObj[rows[x].dataset_id].api_types[rows[x].api_type_id] = {"api_type_id": rows[x].api_type_id, "name": rows[x].apiname};
+                if( rows[x].project_id != null ) holderObj[rows[x].dataset_id].projects[rows[x].project_id] = {"project_id": rows[x].project_id, "name": rows[x].project_name, "descritiption": rows[x].projectdescritiption};
+                if( rows[x].problem_name != null ) holderObj[rows[x].dataset_id].problems[rows[x].problem_id] = {"problem_id": rows[x].problem_id, "name": rows[x].problem_name, "descritiption": rows[x].problem_description};
 
                 delete holderObj[rows[x].dataset_id].api_type_id;
                 delete holderObj[rows[x].dataset_id].apiname;
@@ -116,9 +113,9 @@ function getDataSet( res, id )
             }
             else
             {
-                if( rows[x].api_type_id != null ) holderObj[rows[x].dataset_id].api_types[rows[x].api_type_id] = [{"api_type_id": rows[x].api_type_id, "name": rows[x].apiname}];
-                if( rows[x].project_id != null ) holderObj[rows[x].dataset_id].projects[rows[x].project_id] = [{"project_id": rows[x].project_id, "name": rows[x].project_name, "descritiption": rows[x].projectdescritiption}];
-                if( rows[x].problem_name != null ) holderObj[rows[x].dataset_id].problems[rows[x].problem_id] = [{"problem_id": rows[x].problem_id, "name": rows[x].problem_name, "descritiption": rows[x].problem_description}];
+                if( rows[x].api_type_id != null ) holderObj[rows[x].dataset_id].api_types[rows[x].api_type_id] = {"api_type_id": rows[x].api_type_id, "name": rows[x].apiname};
+                if( rows[x].project_id != null ) holderObj[rows[x].dataset_id].projects[rows[x].project_id] = {"project_id": rows[x].project_id, "name": rows[x].project_name, "descritiption": rows[x].projectdescritiption};
+                if( rows[x].problem_name != null ) holderObj[rows[x].dataset_id].problems[rows[x].problem_id] = {"problem_id": rows[x].problem_id, "name": rows[x].problem_name, "descritiption": rows[x].problem_description};
             }
         }
 
@@ -126,6 +123,64 @@ function getDataSet( res, id )
         for(var key in holderObj) {
             holderObj[key].api_types = removeKeys(holderObj[key].api_types);
             holderObj[key].projects = removeKeys(holderObj[key].projects);
+            holderObj[key].problems = removeKeys(holderObj[key].problems);
+        }
+
+        res.send(removeKeys(holderObj));
+    });
+}
+
+function getProjects( res, id )
+{
+    var query = 'SELECT projects.*, datasets_to_api_types.api_type_id, api_types.name AS apiname, users.name AS contributer, creators.name AS creator, datasets.dataset_id, datasets.name AS dataset_name, datasets.description AS dataset_description, problems.problem_id, problems.name AS problem_name, problems.description AS problem_descrip FROM projects LEFT JOIN projects_to_datasets ON projects.project_id = projects_to_datasets.project_id LEFT JOIN datasets ON projects_to_datasets.dataset_id = datasets.dataset_id LEFT JOIN datasets_to_api_types ON datasets_to_api_types.dataset_id = datasets.dataset_id LEFT JOIN api_types ON datasets_to_api_types.api_type_id = datasets_to_api_types.api_type_id LEFT JOIN users ON projects.contributer_id = users.user_id LEFT JOIN creators ON projects.creator_id = creators.creator_id LEFT JOIN problems_to_projects ON problems_to_projects.project_id = projects.project_id LEFT JOIN problems ON problems.problem_id = problems_to_projects.problem_id';
+    if( id !== undefined ) query += ' WHERE projects.project_id = '+ id;
+
+    connection.query(query, function(err, rows, fields) {
+        if( err ) console.log('error: ' + err);
+
+        var holderObj = {};
+        for( var x = 0; x < rows.length; x++ )
+        {
+            if( holderObj[rows[x].project_id] == null )
+            {
+                holderObj[rows[x].project_id] = rows[x];
+                holderObj[rows[x].project_id].datasets = {};
+                holderObj[rows[x].project_id].problems = {};
+
+                if( rows[x].dataset_id != null )
+                {
+                    holderObj[rows[x].project_id].datasets[rows[x].dataset_id] = {"dataset_id": rows[x].dataset_id, "name": rows[x].name, "description": rows[x].dataset_description, "api_types":{} };
+                    if( rows[x].api_type_id != null ) holderObj[rows[x].project_id].datasets[rows[x].dataset_id].api_types[rows[x].api_type_id] = {"api_type_id": rows[x].api_type_id, "name": rows[x].apiname};
+                }
+                if( rows[x].problem_name != null ) holderObj[rows[x].project_id].problems[rows[x].problem_id] = {"problem_id": rows[x].problem_id, "name": rows[x].problem_name, "descritiption": rows[x].problem_description};
+
+                delete holderObj[rows[x].project_id].api_type_id;
+                delete holderObj[rows[x].project_id].apiname;
+                delete holderObj[rows[x].project_id].problem_id;
+                delete holderObj[rows[x].project_id].problem_name;
+                delete holderObj[rows[x].project_id].problem_description;
+                delete holderObj[rows[x].project_id].dataset_id;
+                delete holderObj[rows[x].project_id].dataset_name;
+                delete holderObj[rows[x].project_id].dataset_description;
+            }
+            else
+            {
+                if( rows[x].dataset_id != null )
+                {
+                    holderObj[rows[x].project_id].datasets[rows[x].dataset_id] = {"dataset_id": rows[x].dataset_id, "name": rows[x].name, "description": rows[x].dataset_description, "api_types":{} };
+                    if( rows[x].api_type_id != null ) holderObj[rows[x].project_id].datasets[rows[x].dataset_id].api_types[rows[x].api_type_id] = {"api_type_id": rows[x].api_type_id, "name": rows[x].apiname};
+                }
+                if( rows[x].problem_name != null ) holderObj[rows[x].project_id].problems[rows[x].problem_id] = {"problem_id": rows[x].problem_id, "name": rows[x].problem_name, "descritiption": rows[x].problem_description};
+            }
+        }
+
+        for(var key in holderObj) {
+            holderObj[key].datasets = removeKeys(holderObj[key].datasets);
+
+            for(var key2 in holderObj[key].datasets) {
+                holderObj[key].datasets[key2].api_types = removeKeys(holderObj[key].datasets[key2].api_types);
+            }
+
             holderObj[key].problems = removeKeys(holderObj[key].problems);
         }
 
